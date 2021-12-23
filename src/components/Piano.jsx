5c33fb59647b39344debf6737b52f41e33076313
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2021-12-23 16:05:04 -0500

    Replace WhiteKey and BlackKey component with generic Key component

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import BlackKey from "./BlackKey";
-import WhiteKey from "./WhiteKey";
+import Key from "./Key";
 import "./Piano.css"
 
 const Piano = () => {
@@ -8,23 +7,23 @@ const Piano = () => {
     <div className="piano">
       <div className="black-keys">
         <div className="C-D-key-group">
-          <BlackKey keyCode="r" audioId="Csharp3" />
-          <BlackKey keyCode="t" audioId="Dsharp3" />
+          <Key keyColor="black" keyCode="r" audioId="Csharp3" />
+          <Key keyColor="black" keyCode="t" audioId="Dsharp3" />
         </div>
         <div className="F-G-A-key-group">
-          <BlackKey keyCode="u" audioId="Fsharp3" />
-          <BlackKey keyCode="i" audioId="Gsharp3" />
-          <BlackKey keyCode="o" audioId="Asharp3" />
+          <Key keyColor="black" keyCode="u" audioId="Fsharp3" />
+          <Key keyColor="black" keyCode="i" audioId="Gsharp3" />
+          <Key keyColor="black" keyCode="o" audioId="Asharp3" />
         </div>
       </div>
       <div className="white-keys">
-        <WhiteKey keyCode="d" audioId="C3" />
-        <WhiteKey keyCode="f" audioId="D3" />
-        <WhiteKey keyCode="g" audioId="E3" />
-        <WhiteKey keyCode="h" audioId="F3" />
-        <WhiteKey keyCode="j" audioId="G3" />
-        <WhiteKey keyCode="k" audioId="A3" />
-        <WhiteKey keyCode="l" audioId="B3" />
+        <Key keyColor="white" keyCode="d" audioId="C3" />
+        <Key keyColor="white" keyCode="f" audioId="D3" />
+        <Key keyColor="white" keyCode="g" audioId="E3" />
+        <Key keyColor="white" keyCode="h" audioId="F3" />
+        <Key keyColor="white" keyCode="j" audioId="G3" />
+        <Key keyColor="white" keyCode="k" audioId="A3" />
+        <Key keyColor="white" keyCode="l" audioId="B3" />
       </div>
     </div>
   )
